efc5a4a48cf965e045cfee38b3ba3404a211302e
Luís Galete <luisgalete6264@gmail.com>
2020-09-21 10:03:42 -0300

    Add base files

---
+++ templates/footer.php
@@ -0,0 +1,5 @@
+<hr>
+<footer class="text-center position-relative">
+  <p>Agenda desenvolvida durante aula - SENAI</p>
+  <p>Aperfeiçoado por <a href="https://github.com/yunger7/" target="_blank">Luís Galete</a></p>
+</footer>
