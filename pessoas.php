1f29da9fb0d81feca44f7c75053fb083985e57f6
Luís Galete <luisgalete6264@gmail.com>
2020-09-27 14:39:59 -0300

    Update pessoas.php

--- pessoas.php
+++ pessoas.php
@@ -94,7 +94,7 @@ mysqli_close($conn);
     </ul>
     <form action="config/procurar.php" method="GET" class="mr-4">
       <div class="input-group">
-        <input type="text" name="nome-procurar" class="form-control" placeholder="Digite um nome">
+        <input type="text" name="nome-procurar" class="form-control" placeholder="Digite um nome" required>
         <div class="input-group-append">
           <button type="submit" class="btn btn-outline-info">
             <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
