d53115e5f5f57da398d859e9cace690bad6a14fa
Luís Galete <luisgalete6264@gmail.com>
2021-02-10 10:15:44 -0300

    Handle login errors

--- v2.0 - React/src/components/Login.jsx
+++ v2.0 - React/src/components/Login.jsx
@@ -9,7 +9,9 @@ import {
 	makeStyles,
 	TextField,
 	Button,
+	Snackbar
 } from "@material-ui/core";
+import { Alert } from "@material-ui/lab"
 import { AccountCircle } from "@material-ui/icons";
 
 // Contexts
@@ -40,55 +42,94 @@ const useStyles = makeStyles({
 
 const Login = () => {
 	const classes = useStyles();
+	const { login } = useContext(AuthContext);
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
-	const { login } = useContext(AuthContext);
+	const [error, setError] = useState({ error: false, component: '', message: '' });
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
-		login(email, password);
+
+		// validation
+		if (!email.length) {
+			return setError({ error: true, component: 'email', message: 'Insira um email' });
+		}
+		if (!password.length) {
+			return setError({ error: true, component: 'password', message: 'Insira uma senha' });
+		}
+
+		login(email, password)
+		.then(() => {
+			setError({ error: false, component: '', message: '' });
+		})
+		.catch(error => {
+			console.log(error);
+			if (error.code === "auth/wrong-password") {
+				setError({ error: true, component: 'snackbar', message: 'Senha inválida' });
+			}
+			if (error.code === "auth/user-not-found") {
+				setError({ error: true, component: 'snackbar', message: 'Usuário não encontrado' });
+			}
+		});
 	}
 
 	return (
-		<Container component="main" maxWidth="sm">
-			<Grid
-				container
-				direction="column"
-				alignItems="center"
-				className={classes.wrapper}
-			>
-				<AccountCircle style={{ fontSize: 45 }} />
-				<Typography variant="h1">Sistema de agenda</Typography>
-				<form className={classes.form} onSubmit={handleSubmit}>
-					<TextField
-						type="email"
-						label="Email"
-						required
-						className={classes.input}
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-					/>
-					<TextField
-						type="password"
-						label="Senha"
-						required
-						className={classes.input}
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-					/>
-					<Button
-						type="submit"
-						variant="contained"
-						color="primary"
-						size="medium"
-						className={classes.button}
-					>
-						Entrar
-					</Button>
-				</form>
-				<Link to="/signup" className={classes.link}>Don't have an account? Sign up instead!</Link>
-			</Grid>
-		</Container>
+		<>
+			<Container component="main" maxWidth="sm">
+				<Grid
+					container
+					direction="column"
+					alignItems="center"
+					className={classes.wrapper}
+				>
+					<AccountCircle style={{ fontSize: 45 }} />
+					<Typography variant="h1">Sistema de agenda</Typography>
+					<form className={classes.form} onSubmit={handleSubmit}>
+						<TextField
+							type="email"
+							label="Email"
+							className={classes.input}
+							value={email}
+							onChange={e => setEmail(e.target.value)}
+							error={error.component === 'email'}
+							helperText={error.component === 'email' && error.message}
+						/>
+						<TextField
+							type="password"
+							label="Senha"
+							className={classes.input}
+							value={password}
+							onChange={e => setPassword(e.target.value)}
+							error={error.component === 'password'}
+							helperText={error.component === 'password' && error.message}
+						/>
+						<Button
+							type="submit"
+							variant="contained"
+							color="primary"
+							size="medium"
+							className={classes.button}
+						>
+							Entrar
+						</Button>
+					</form>
+					<Link to="/signup" className={classes.link}>Não possui cadastro? Cadastre-se</Link>
+				</Grid>
+			</Container>
+
+			<Snackbar
+			anchorOrigin={{
+				vertical: 'top',
+				horizontal: 'center',
+			}}
+			open={error.component === "snackbar"}
+			autoHideDuration={6000}
+		>
+			<Alert severity="error">
+				{ error.component === "snackbar" && error.message }
+			</Alert>
+		</Snackbar>
+	</>
 	);
 };
 
